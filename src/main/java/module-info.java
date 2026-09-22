2ed34c3d55c83981ad1f6814d79c565d052f82bd
moraditoo <damora@uamv.edu.ni>
2026-09-22 16:40:39 -0600

    corrige error lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,15 @@ module ni.edu.uam._corteexamen {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // ESTA LÍNEA ES LA QUE FALTA O RESUELVE EL ERROR:
+    requires static lombok;
 
+    // Accesos y aperturas para JavaFX y PropertyValueFactory de la tabla
     opens ni.edu.uam._corteexamen to javafx.fxml;
+    opens ni.edu.uam._corteexamen.controller to javafx.fxml;
+    opens ni.edu.uam._corteexamen.model to javafx.base;
+
     exports ni.edu.uam._corteexamen;
+    exports ni.edu.uam._corteexamen.controller;
+    exports ni.edu.uam._corteexamen.model;
 }
